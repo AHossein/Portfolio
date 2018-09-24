2fa0009a182e2d5a917d6f152442fb2bff642df8
Hossein <hossein.ahmed78@gmail.com>
2018-09-24 18:23:44 +0200

    Responsive/bug correction

--- js/script.js
+++ js/script.js
@@ -16,45 +16,45 @@ jQuery(function ($)
   });
 
 
-// TYPED JS
-$(document).ready(function(){
-  var typed = new Typed('#typed', {
-    stringsElement: '#typed-strings',
-    backSpeed: 40,
-    typeSpeed: 90,
-    loop: true
+  // TYPED JS
+  $(document).ready(function(){
+    var typed = new Typed('#typed', {
+      stringsElement: '#typed-strings',
+      backSpeed: 40,
+      typeSpeed: 90,
+      loop: true
+    });
   });
-});
 
 
-// SMOOTHSCROLL PLUGIN
-var scrollAbout = new SmoothScroll('a[href*="#about"]', {
-  speed: 1500
-});
-var scrollExp = new SmoothScroll('a[href*="#experience"]', {
-  speed: 1500
-});
-var scrollEduc = new SmoothScroll('a[href*="#education"]', {
-  speed: 1500
-});
-var scrollAbilities = new SmoothScroll('a[href*="#abilities"]', {
-  speed: 1500
-});
-var scrollInter = new SmoothScroll('a[href*="#interests"]', {
-  speed: 1500
-});
-var scrollContact = new SmoothScroll('a[href*="#contact"]', {
-  speed: 1500
-});
+  // SMOOTHSCROLL PLUGIN
+  var scrollAbout = new SmoothScroll('a[href*="#about"]', {
+    speed: 1500
+  });
+  var scrollExp = new SmoothScroll('a[href*="#experience"]', {
+    speed: 1500
+  });
+  var scrollEduc = new SmoothScroll('a[href*="#education"]', {
+    speed: 1500
+  });
+  var scrollAbilities = new SmoothScroll('a[href*="#abilities"]', {
+    speed: 1500
+  });
+  var scrollInter = new SmoothScroll('a[href*="#interests"]', {
+    speed: 1500
+  });
+  var scrollContact = new SmoothScroll('a[href*="#contact"]', {
+    speed: 1500
+  });
 
 
 
-// HIDDEN NAVBAR
-var nav = document.getElementById('navAccess'),
-    body = document.body;
+  // HIDDEN NAVBAR
+  var nav = document.getElementById('navAccess'),
+  body = document.body;
 
-nav.addEventListener("click", function(e)
-{
+  nav.addEventListener("click", function(e)
+  {
     body.className = body.className? '' : 'with_nav';
     e.preventDefault();
-});
+  });
